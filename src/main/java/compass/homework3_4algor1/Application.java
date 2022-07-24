c87a47700fddecaafcb3b750211978852c7a7884
gerasimov707 <gerasimov707@yandex.ru>
2022-07-24 19:46:40 +0300

    коммит

---
+++ src/main/java/compass/homework3_4algor1/Application.java
@@ -0,0 +1,13 @@
+package compass.homework3_4algor1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/compass/homework3_4algor1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package compass.homework3_4algor1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
